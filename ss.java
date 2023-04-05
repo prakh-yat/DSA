fe64afe14a5a2d9e7dbe76d2b3e9203368cda6dc
Prakhyat Shrestha <81764309+prakh-yat@users.noreply.github.com>
2023-04-05 22:12:16 +0530

    Create ss.java

---
+++ ss.java
@@ -0,0 +1,22 @@
+class ss{
+	public static void main(String[] args){
+		int[] a = {38,52,9,18,6,62,13};
+
+		int min, temp=0;
+		for ( int i = 0; i<a.length ;i++){
+			min = i;
+			for ( int j=i+1 ; j<a.length ; j++){
+				if ( a[j] < a[min]){
+					min = j;
+				}
+			}
+			temp = a[i];
+			a[i] = a[min];
+			a[min] = temp;
+		}
+		for ( int k=0 ; k<a.length ; k++){
+			System.out.println(a[k]);
+		}
+
+	}
+}
