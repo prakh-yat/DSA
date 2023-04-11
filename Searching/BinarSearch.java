a86a5f9074b240ce1558d230576a58911a74e42f
Prakhyat Shrestha <81764309+prakh-yat@users.noreply.github.com>
2023-04-11 21:54:35 +0530

    Create BinarSearch.java

---
+++ Searching/BinarSearch.java
@@ -0,0 +1,31 @@
+public class BinarySearch {
+
+    public static int binarySearch(int[] arr, int target) {
+        return binarySearch(arr, target, 0, arr.length - 1);
+    }
+
+    private static int binarySearch(int[] arr, int target, int low, int high) {
+        if (low > high) {
+            return -1;
+        }
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] > target) {
+            return binarySearch(arr, target, low, mid - 1);
+        } else {
+            return binarySearch(arr, target, mid + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9};
+        int target = 7;
+        int result = binarySearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element not present");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+}
